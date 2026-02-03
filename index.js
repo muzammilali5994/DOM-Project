fbcdbb296bba9b5af8208e784fcd0995fa98c608
muzammilali5994 <muzammilali5994@outlook.com>
2026-02-03 13:43:18 +0500

    digital time complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const track = document.getElementById("banner");
 let position = 0;
 
 function moveRight() {
-    position -= 0.5; // speed
+    position -= 1; // speed
 
     track.style.transform = `translateX(${position}px)`;
 
@@ -18,3 +18,20 @@ function moveRight() {
 if (track) {
     moveRight();
 }
+
+
+const endTime = new Date("Feb 20, 2026 23:59:59").getTime();
+
+const now = new Date().getTime();
+
+const diff = endTime - now ;
+
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+const minutes = Math.floor((diff / (1000 * 60)) % 60);
+const seconds = Math.floor((diff / 1000) % 60);
+
+document.getElementById("days").innerText = days;
+document.getElementById("hours").innerText = hours;
+document.getElementById("minutes").innerText = minutes;
+document.getElementById("seconds").innerText = seconds;
